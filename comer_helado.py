61462697df50fcfd2b0821725c70fa43bf6cbb72
mochilive <mochilive97@gmail.com>
2018-05-15 01:52:12 +0200

    añadimos .upper()

--- comer_helado.py
+++ comer_helado.py
@@ -1,12 +1,12 @@
 
-apetece_helado_input = input("te apetece un helado? (si / no): ")
-tienes_dinero_input = input("tienes dinero para comprarlo? (si / no ): ")
-esta_senor_helados_input = input("¿esta el señor de los helados? (si / no): ")
-esta_tu_tia_input = input("estas con tu tia? (si / no): ")
+apetece_helado_input = input("te apetece un helado? (si / no): ").upper()
+tienes_dinero_input = input("tienes dinero para comprarlo? (si / no ): ").upper()
+esta_senor_helados_input = input("¿esta el señor de los helados? (si / no): ").upper()
+esta_tu_tia_input = input("estas con tu tia? (si / no): ").upper()
 
-apetece_helado = apetece_helado_input == "si"
-puede_permitirselo = tienes_dinero_input == "si" or esta_tu_tia_input == "si"
-esta_senor_helados = esta_senor_helados_input == "si"
+apetece_helado = apetece_helado_input == "SI"
+puede_permitirselo = tienes_dinero_input == "SI" or esta_tu_tia_input == "SI"
+esta_senor_helados = esta_senor_helados_input == "SI"
 
 if apetece_helado and puede_permitirselo and esta_senor_helados:
     print("Pues comete un helado")
