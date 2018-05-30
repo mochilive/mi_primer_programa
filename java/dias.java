02775f648257b51926d8818e26d1a201618c739b
mochilive <mochilive97@gmail.com>
2018-05-30 18:47:11 +0200

    muestra los dias de la semana en fila "print"

---
+++ java/dias.java
@@ -0,0 +1,11 @@
+public class dias{
+
+    public static void main(String[] args){
+
+        String[] diasSemana = {"Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","Domingo"};
+        for(int x = 0; x <= diasSemana.length-1; x++){
+            System.out.print(diasSemana[x] + ",");
+
+        }
+    }
+}
