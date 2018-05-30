8bd3147bc30b92f385a210ad033b0df1d18feec1
mochilive <mochilive97@gmail.com>
2018-05-30 12:51:53 +0200

    modificado

--- Pruebas/mayor_de_tres.py
+++ Pruebas/mayor_de_tres.py
@@ -2,6 +2,14 @@ n1 = int(input("Primer numero: "))
 n2 = int(input("Segundo numero: "))
 n3 = int(input("Tercer numero: "))
 
-resultado = max(n1, n2, n3)
+if n1 > n2 and n1 > n3:
+    resultado = n1
+    print("El mayor es: {}".format(resultado))
 
-print("El numero mas grande es {}" .format(resultado))
+elif n2 > n1 and n2 > n3:
+    resultado = n2
+    print("El mayor es: {}".format(resultado))
+
+elif n3 > n1 and n3 > n2:
+    resultado = n3
+    print("El mayor es: {}".format(resultado))
