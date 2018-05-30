9419e4536c08b0c20b72ee5251306cf1fa912957
mochilive <mochilive97@gmail.com>
2018-05-30 17:50:53 +0200

    paquetes dentro de paquetes

---
+++ java/prueba/avanzadas/HolaMundodos.java
@@ -0,0 +1,13 @@
+package prueba.avanzadas;
+
+public class HolaMundodos{
+
+    public static void main(String args []){
+
+        String[] mes = {"enero","febrero","marzo","abril","mayo","junio","julio","agosto","septiembre","octubre","noviembre","diciebre"};
+        for(int x = 0; x <=mes.length-1; x++){
+            System.out.println(mes[x]);
+        }
+
+    }
+}
